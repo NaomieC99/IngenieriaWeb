04724f989408f0d4224d0a7d9fc0f55768fce210
Naomie Contreras <naomie.contreras.q@mail.pucv.cl>
2022-12-05 00:13:18 -0300

    UPDATE

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PacientesModule } from './pacientes/pacientes.module';
-import { MongoModule } from './config/mongo.module';
-import { BooksModule } from './books/books.module';
+import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { BooksModule } from './books/books.module';
 
 @Module({
-  imports: [PacientesModule, MongoModule, BooksModule, UsersModule],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost:27017/API'), 
+    UsersModule,
+    BooksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
